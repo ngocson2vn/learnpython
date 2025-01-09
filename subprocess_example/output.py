d18b6dd5248078b3a337ae18460bf94ff4f45398
Sony Nguyen <son.nguyen@bytedance.com>
2025-01-09 12:45:23 +0800

    Update

---
+++ subprocess_example/output.py
@@ -0,0 +1,9 @@
+import subprocess as sp
+
+cmd="while true; do echo 'Hello!'; sleep 1; done"
+
+p = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.STDOUT, shell=True, bufsize=1, encoding="utf-8")
+for line in p.stdout:
+    print(line, end="")
+p.stdout.close()
+p.wait()
